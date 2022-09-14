caff2f1bddedeb2a36492203b88efdf4f56d71cc
Gabriel Perez <gabrielpaa94@gmail.com>
2022-09-14 18:03:34 -0500

    first commit

---
+++ views/layout.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Prueba Tecnica C&A</title>
+  <link rel="shortcut icon" href="../build/img/cya.jpg" type="image/x-icon">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;700;900&display=swap" rel="stylesheet"> 
+    <link rel="stylesheet" href="build/css/app.css">
+    <script src="https://kit.fontawesome.com/cd5910c92f.js" crossorigin="anonymous"></script>
+</head>
+<body>
+
+    <?php echo $contenido; ?>
+            
+
+</body>
+</html>
---
+++ views/paginas/inicio.php
@@ -0,0 +1,64 @@
+<div class="header">
+    <div class="header__imagen">
+        <img src="../build/img/cya.jpg" alt="imagen de C&A">
+    </div>  
+    <div class="header__contenido">
+        <a href="/">Hombre</a>
+        <a href="/">Mujer</a>
+        <a href="/">Mujer</a>
+        <a href="/">Niños</a>
+        <a href="/">Calzado</a>
+    </div>  
+
+</div>
+
+<section class="hero">
+    <img src="../build/img/imagenGrande.webp" alt="imagen grande">
+</section>
+
+<section class="contenedor">
+    <div class="menu-oculto">
+        <i class="fa-solid fa-bars"></i>
+        <div class="group">
+            <i class="fa-solid fa-magnifying-glass"></i>
+            <i class="fa-sharp fa-solid fa-cart-shopping"></i>
+        </div>
+    </div>
+    <h2>Hombre (cantidad)</h2>
+</section>
+
+<section class="contenedor cards ">
+
+    <div class="card">
+        <div class="card__corazon">
+        <i class="fa-regular fa-heart"></i>
+        </div>
+        <img src="../build/img/camisa_negra.webp" alt="imagen de camisa" >
+        <p>Titulo playeras</p>
+        <p> <span>precio $350.00</span> </p>
+    </div>
+    <div class="card">
+        <div class="card__corazon">
+           <i class="fa-regular fa-heart"></i>
+        </div>
+        <img src="../build/img/camisa_negra.webp" alt="imagen de camisa" >
+         <p>Titulo playeras</p>
+        <p> <span>precio $350.00</span> </p>
+    </div>
+    <div class="card">
+        <div class="card__corazon">
+           <i class="fa-regular fa-heart"></i>
+        </div>
+        <img src="../build/img/camisa_negra.webp" alt="imagen de camisa" >
+        <p>Titulo playeras</p>
+        <p> <span>precio $350.00</span> </p>
+    </div>
+    <div class="card">
+        <div class="card__corazon">
+           <i class="fa-regular fa-heart"></i>
+        </div>
+        <img src="../build/img/camisa_negra.webp" alt="imagen de camisa" >
+         <p>Titulo playeras</p>
+        <p> <span>precio $350.00</span> </p>
+    </div>
+</section>
